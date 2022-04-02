98798e5d64ab4c2e290016252c3e8ebea9b8d75f
huseyin.gungor <hgungor0694@gmail.com>
2022-04-03 01:03:26 +0300

    Rover servisi eklendi ve rover oluşturma tamamlandı. Plateau servis iyileştirmeleri yapıldı.

---
+++ MarsRover.Common/Enumarations/RoverEnums.cs
@@ -0,0 +1,26 @@
+﻿namespace MarsRover.Common.Enumarations
+{
+    public class RoverEnums
+    {
+        public static class Directions
+        {
+            public const string North = "N";
+            public const string East = "E";
+            public const string South = "S";
+            public const string West = "W";
+        }
+
+        public static class Errors
+        {
+            public const string EmptyParameters = "Rover parameters are not valid! Try again.";
+            public const string WrongParameters = "Rover cannot be created! Try again.";
+            public const string LocationNotInPlateau = "Rover location is not in plateau! Try again.";
+            public const string NotValidRoverPosition = "Rover position is not valid! Try again.";
+        }
+
+        public enum Properties
+        {
+            ParametersCount = 3
+        }
+    }
+}
---
+++ MarsRover.Models/Rover.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRover.Models
+{
+    public class Rover
+    {
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public string Direction { get; set; }
+
+        public Rover(int xPosition, int yPosition, string direction)
+        {
+            XPosition = xPosition; 
+            YPosition = yPosition; 
+            Direction = direction;
+        }
+    }
+}
